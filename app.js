20b99aa952ca63abf7b98f79d68813d209fc74d4
Sergeich3 <izsergeev2@gmail.com>
2022-11-11 19:25:50 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,239 @@
+// let age = 5 / 0;
+// const gender = 'муж'  // жен
+
+// if (gender === 'муж')  {
+//     console.log ('Вам налево')
+// }
+
+// if (gender,toLocaleLowerCase() === 'муж') {
+//     console.log ('Вам налево')
+// }
+
+// if (gender === 'жен')   {
+//     console.log ('Вам направо')
+// }
+   
+
+// if (!isNaN(age)) {
+//     if (age > 18) {
+//         console.log ("Вам больше 18");
+//     } else if (age < 18) {
+//         console.log ('Вам меньше 18 лет');
+//     } else if (age === 18) {
+//         console.log ('Вам исполнилось 18 лет');
+//     } else {
+//         console.log('Укажите Ваш возраст');
+//     }
+// } else {
+//     console.log ('Введите корректные данные');
+// }
+
+
+// const typeUser = 'admin' // user, guest, bot
+
+// switch (typeUser) {
+//     case 'admin' :
+//         console.log ('Вы администратор');
+//         break;
+//     case 'user':
+//         console.log('Вы пользователь');
+//         break;
+//     case 'guest':
+//         console.log ('Вы гость');
+//         break;
+//     default:
+//         console.log('Неизвестный тип пользователя')
+//         break
+// }
+
+
+// //вывести на экран числа от 1до 10 через запятую
+
+// let i = 1;
+// const lastNumber = 10;
+// let message = '';
+
+// while (i <= lastNumber) {
+//     message = message + `${i},`;
+
+//     // message +=  i + ',' ;
+
+//     i++;
+// }
+
+// console.log(message);
+
+
+
+// домашняя работа
+
+// let a = 0;
+// if (a == 0) {
+// console.log('Right');
+// } else {
+// console.log('Wrong');
+// };
+
+
+// let a = 1;
+// if (a > 0) {
+//     console.log('Right!');
+// } else {
+//     console.log('Wrong');
+// }
+
+// let a = -1;
+// if (a < 0) {
+// console.log('Right!');
+// } else {
+// console.log('Wrong');
+
+// }
+
+// let a = 1;
+// if (a >= 0) {
+// console.log('Right!');
+// } else {
+// console.log('Wrong');
+
+
+// let a = 1;
+// if (a != 0) {
+// console.log('Right!');
+// } else {
+// console.log('Wrong');
+
+
+// let a = 'test';
+// if (a === 'test') {
+// console.log('Right!');
+// } else {
+// console.log('Wrong')}
+
+// let a = '1';
+// if (a == '1') {
+// console.log('Right!');
+// } else {
+// console.log('Wrong');
+// };
+
+//Работа с логическими переменными
+
+// let test = true;
+// if (test) {
+//     console.log ('Right')
+// }else {
+//     console.log ('Wrong')
+// }
+
+// let test = true
+// console.log(test ? 'right': 'wrong')
+
+
+// let test = true;
+// if (!test) {
+//     console.log ('Right')
+// }else {
+//     console.log ('Wrong')
+// }
+
+// let test = true
+// console.log(!test ? 'right': 'wrong')
+
+// Работа с && (и) и || (или)
+
+// let a = 2;
+// if (a > 0 && a < 5) {
+//     console.log('right')
+// }else {
+//     console.log('wrong')
+// }
+
+// let a = 9;
+// if (a == 0 || a == 2) {
+//     a = a + 7 ;
+// }else {
+//     a = a / 10;
+// }
+
+// console.log(a)
+
+// let a = 1
+// let b = 3
+// if ((a = 1 || a < 1) && (b = 3 || b > 3)) {
+//     console.log(a+b)
+// }else {
+//     console.log(a-b)
+// }
+
+// let num = 4
+// let result = ''
+// switch(num){
+//     case 1 : 
+//     result = 'зима'
+//     break
+//     case 2 :
+//     result = 'весна'
+//     break
+//     default : 
+//     result = 'false'
+// }
+// console.log(result)
+
+
+// let day = 30
+// let dekada = 0
+//   if (day <= 31 && day > 20) {
+//     dekada = 3
+//   }else if (day < 20 && day > 10) {
+//     dekada = 2
+//   }else if (day < 10 && day > 0) {
+//     dekada = 1
+//   }
+//   console.log(dekada)
+
+
+
+// let month = new Date().getMonth() + 1;
+// let season = ''
+// if (month == 12 || month >= 1 && month <= 2) {
+//     season = 'зима'
+// }else if (month >= 3 && month <= 5) {
+//     season = 'весна'
+// }else if (month >= 6 && month <= 8) {
+//     season = 'лето'
+// }else if (month >=9  && month <= 11) {
+//     season = 'осень'
+// }
+// console.log(season)
+
+//1970 UTC
+
+
+
+// let i = 1;
+// const lastNumber = 10;
+// let message = '';
+
+
+// }
+
+// let i = 11 //2 //3 //4 //5
+// let lastNumber = 33
+
+// while (i <= lastNumber) {
+//     //Итерация
+//      // инкремент 2
+//     console.log(i); //1 //2 //3 //4
+//     i++;
+   
+// }
+ 
+for(let i = 1; i <= 100; i++) {
+    
+    if( !(i%2) ){ //false /true
+        console.log(i)
+    }
+}
+
+//1
